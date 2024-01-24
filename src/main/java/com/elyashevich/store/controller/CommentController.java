0654b0b24a3ec6b6b4cde67e0e63c3c176b60ebc
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-01-24 23:04:11 +0300

    fix: add slfj

--- src/main/java/com/elyashevich/store/controller/CommentController.java
+++ src/main/java/com/elyashevich/store/controller/CommentController.java
@@ -6,11 +6,13 @@ import com.elyashevich.store.entity.Comment;
 import com.elyashevich.store.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/comments")
 @RequiredArgsConstructor
